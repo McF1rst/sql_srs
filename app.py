cfc13ce4efe8195775f93a6c724e3ec0b65a9d8e
McF1rst <matthieu2209@sfr.fr>
2025-03-02 16:17:44 +0100

    first streamlit app

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+# Création du DataFrame
+data = {
+    "Colonne_A": [1, 2, 3, 4, 5],
+    "Colonne_B": ["A", "B", "C", "D", "E"],
+    "Colonne_C": [10.5, 20.3, 30.1, 40.8, 50.2]
+}
+
+df = pd.DataFrame(data)
+tab1, tab2, tab3 = st.tabs(['Cat', 'Dog', 'Owl'])
+with tab1:
+    input_text = st.text_area('Entrez votre input')
+    processed_dataframe = duckdb.sql(input_text)
+    if input_text:
+        st.dataframe(processed_dataframe)
