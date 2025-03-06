f345a7812a12a8577842811d6bd5899ae442c57e
McF1rst <matthieu2209@sfr.fr>
2025-03-06 10:56:26 +0100

    Reformat file

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
+# pylint: disable=missing-module-docstring
+import io
 import streamlit as st
 import pandas as pd
 import duckdb
-import io
 
 st.title("SQL SRS")
 # Création du DataFrame
-csv = '''
+CSV = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
-'''
+"""
 
-beverages = pd.read_csv(io.StringIO(csv))
-csv2 = '''
+beverages = pd.read_csv(io.StringIO(CSV))
+CSV2 = """
 food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
-'''
+"""
 
-food_items = pd.read_csv(io.StringIO(csv2))
-answer = """
+food_items = pd.read_csv(io.StringIO(CSV2))
+ANSWER = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
-solution = duckdb.sql(answer).df()
+solution = duckdb.sql(ANSWER).df()
 with st.sidebar:
-    option = st.selectbox("What would you like to review ?", ['Joins', 'Group By', 'Windows Functions'],
-                          placeholder="select a value ...", index=None)
+    option = st.selectbox(
+        "What would you like to review ?",
+        ["Joins", "Group By", "Windows Functions"],
+        placeholder="select a value ...",
+        index=None,
+    )
     if option:
-        st.write('You selected ', option)
-input_text = st.text_area('Entrez votre input')
+        st.write("You selected ", option)
+input_text = st.text_area("Entrez votre input")
 col1, col2 = st.columns(2)
 if input_text:
     processed_dataframe = duckdb.sql(input_text).df()
@@ -46,7 +51,7 @@ if input_text:
             st.error("But your output is :")
             st.dataframe(processed_dataframe)
 
-tab1, tab2 = st.tabs(['Tables', 'Solution'])
+tab1, tab2 = st.tabs(["Tables", "Solution"])
 with tab1:
     st.write("table: beverages")
     st.dataframe(beverages)
@@ -55,4 +60,4 @@ with tab1:
     st.write("expected:")
     st.dataframe(solution)
 with tab2:
-    st.write(answer)
+    st.write(ANSWER)
