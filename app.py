3090cbe060e0fdf26d8eaa5008f235dd6cc8eaa2
McF1rst <matthieu2209@sfr.fr>
2025-03-16 19:54:01 +0100

    Add temporal remind

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-module-docstring
 import logging
 import os
-
+from datetime import date, timedelta
 import duckdb
 import streamlit as st
 
@@ -78,10 +78,17 @@ solution = con.execute(answer).df()
 input_text = st.text_area("Entrez votre input")
 col1, col2 = st.columns(2)
 
-
 if input_text:
     check_answer(input_text)
 
+for n_days in [2, 7, 21]:
+    if st.button(f"Revoir dans {n_days} jours"):
+        next_review=date.today() + timedelta(days=n_days)
+        con.execute(f"UPDATE memory_state SET last_reviewed = '{next_review}' WHERE exercise_name='{exercise_to_practice}'")
+        st.rerun()
+if st.button('Reset'):
+    con.execute(f"UPDATE memory_state SET last_reviewed='1970-01-01'")
+    st.rerun()
 tab1, tab2 = st.tabs(["Tables", "Solution"])
 with tab1:
     for table in exercise.loc[
